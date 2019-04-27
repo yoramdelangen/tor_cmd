4b03167580a780f7879c50478fd530fd03c8113f
Yoram de Langen <yoramdelangen@gmail.nl>
2019-04-27 12:21:26 +0200

    Added quiet mode #7

--- cmd.py
+++ cmd.py
@@ -7,8 +7,9 @@ from search import Searcher
 @click.option('--season', '-s', type=int, help='Season number; it will add the searchterm "S{num}" to the search. eg.`-s 1` -> S01')
 @click.option('--episode', '-e', type=int, help='Episode number; it will add the searchterm "S{num}" to the search. eg.`-e 1` -> E01')
 @click.option('--provider', '-p', type=str, default='', help='Search on certain providers: eztv, kickass, thepiratebay, torrentgalaxy. Add a `-` to exclude a provider')
+@click.option('--quiet', '-q', is_flag=True, help='When true this will only show the cache result file relative path. Very usefull for programs that will use this package.')
 
-def tor_cmd(search, season, episode, provider):
+def tor_cmd(search, season, episode, provider, quiet):
 	click.clear()
 
 	show = ''
@@ -21,12 +22,14 @@ def tor_cmd(search, season, episode, provider):
 	if show != '':
 		search += ' '+show
 
-
-
 	# init searcher/webscrape framework
 	s = Searcher(search)
 	p = list(filter(None, provider.split(',')))
 	lp = s.p
+
+	# do we only show the cache filename, cause its quiet
+	if quiet is True:
+		callQuietMode(s, lp, p)
 
 	click.echo('')
 	click.echo('Searching: '+ search)
@@ -60,6 +63,15 @@ def tor_cmd(search, season, episode, provider):
 	click.secho(s.getMagnet(idx), fg='green')
 	click.echo('')
 	click.echo('Do `cmd+click` or `ctrl+click` for opening the magnet url.')
+
+def callQuietMode(s, lp, p):
+	for k in p:
+		del lp[k]
+
+	# search the web
+	s.lookup(lp)
+	click.echo(s.getCacheFilename())
+	exit()
 
 def askForInput(d):
 	idx = click.prompt('Please enter a valid search index', type=int)
